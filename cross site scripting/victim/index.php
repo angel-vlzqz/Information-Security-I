d45015dd7ae31c21a07ecba0e5447230886dad8a
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-12 14:25:13 -0700

    insert index.php

---
+++ cross site scripting/victim/index.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="cite.css">
+    </head>
+    <body>
+        <h1>Post a comment</h1>
+        <form>
+            <label for="comment_title">Comment Title:</label>
+            <input type="text" name="comment_title">
+            <br/>
+            <label for="comment_text">Comment:</label>
+            <textarea name="comment_text" rows="12" cols="70"></textarea>
+            </br>
+            <input type="submit" value="Post comment">
+            </br>
+        </form>
+    
+        <?php
+        if (!$_POST['comment_title'])
+        {
+            die("nothing posted");
+        }
+        ?>
+    
+        <h1>Processing the new post</h1>
+        <div id="postarea">
+            <p>Here is what was posted</p>
+            <?php
+                echo "<pre>";
+                print_r($_POST);
+                echo "</pre>";
+            ?>
+        </div>
+    
+        <h2>New comments</h2>
+            <div id="newcomment">
+                <?php
+                    // this is the vulnerability, we are not checking
+                    // for any threats. we simply accept all text
+                    echo <"<h3>".$_POST['commenttitle']."</h3>";
+                    echo "<p>".$_POST['comment_text']."</p>";
+                ?>
+            </div>
+    </body>
+</html>
