db02a18827b220cc356d39c7f7673437373662f4
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-19 12:36:23 -0700

    pull jokes from table and display onto browser

--- secureweb/index.php
+++ secureweb/index.php
@@ -4,6 +4,7 @@
     <body>
         <h1>Jokes Page</h1>
         <?php
+            // connect to database
             $host = "localhost";
             $username = "root";
             $user_pass = "root";
@@ -15,7 +16,24 @@
             {
                 echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
             }
-            echo $mysqli->host_info . "\n";
+
+            // pull data from test
+            $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table";
+            $result = $mysqli->query($sql);
+
+            if ($result->num_rows > 0)
+            {
+                // output data of each row
+                while ($row = $result->fetch_assoc())
+                {
+                    echo "JokeID: ".$row["JokeID"] ." - Joke Question: ".$row["Joke_question"]." ".$row["Joke_answer"]."<br>";
+                }
+            }
+            else
+            {
+                echo "0 results";
+            }
+            $mysqli->close();
         ?>
     </body>
 </html>
