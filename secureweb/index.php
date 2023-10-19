17c67f0d06cee4e9b57900c3be7f8ed67809695b
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-19 12:22:29 -0700

    connect to MySQL database named test

--- secureweb/index.php
+++ secureweb/index.php
@@ -1,7 +1,21 @@
 <html>
-<head>
-</head>
-<body>
-    Welcome to my web server.
-</body>
+    <head>
+    </head>
+    <body>
+        <h1>Jokes Page</h1>
+        <?php
+            $host = "localhost";
+            $username = "root";
+            $user_pass = "root";
+            $database_in_use = "test";
+
+            $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+            echo $mysqli->host_info . "\n";
+            if ($mysqli->connect_errno) 
+            {
+                echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+            }
+            echo $mysqli->host_info . "\n";
+        ?>
+    </body>
 </html>
