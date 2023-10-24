175e9f025d374d1cdf2689a06558fa7da655f392
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-24 12:02:12 -0700

    add word search functionality to web server

--- secureweb/index.php
+++ secureweb/index.php
@@ -11,7 +11,6 @@
             $database_in_use = "test";
 
             $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
-            echo $mysqli->host_info . "\n";
             if ($mysqli->connect_errno) 
             {
                 echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -19,6 +18,27 @@
 
             // pull data from test
             $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table";
+            $result = $mysqli->query($sql);
+
+            if ($result->num_rows > 0)
+            {
+                // output data of each row
+                while ($row = $result->fetch_assoc())
+                {
+                    echo "JokeID: ".$row["JokeID"] ." - Joke Question: ".$row["Joke_question"]." ".$row["Joke_answer"]."<br>";
+                }
+            }
+            else
+            {
+                echo "0 results";
+            }
+
+
+            // search database for the word stable
+            // pull data from test
+            echo "<h2>Show all jokes with the word Stable in them</h2>";
+
+            $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table WHERE Joke_answer LIKE '%stable%'";
             $result = $mysqli->query($sql);
 
             if ($result->num_rows > 0)
