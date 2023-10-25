dd67e005b2f626f207eb53ddd400d92bcf3ce067
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-24 18:51:06 -0700

    add keyword search functionality

--- secureweb/index.php
+++ secureweb/index.php
@@ -5,8 +5,17 @@
         <h1>Jokes Page</h1>
         <?php
         include "db_connect.php";
-        include "search_all_jokes.php";
-        include "search_keyword.php";
+        // include "search_all_jokes.php";
+        ?>
+
+        <form action="search_keyword.php">
+            Please enter a keyword to search for:<br>
+            <input type="text" name="keyword"><br>
+        </form>
+        <input type="submit" value="Submit">
+
+        <?php
+        // include "search_keyword.php";
 
             $mysqli->close();
         ?>
--- secureweb/search_keyword.php
+++ secureweb/search_keyword.php
@@ -1,21 +1,22 @@
 <?php
-            // search database for the word stable
-            // pull data from test
-            echo "<h2>Show all jokes with the word Stable in them</h2>";
-
-            $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table WHERE Joke_answer LIKE '%stable%'";
-            $result = $mysqli->query($sql);
-
-            if ($result->num_rows > 0)
-            {
-                // output data of each row
-                while ($row = $result->fetch_assoc())
-                {
-                    echo "JokeID: ".$row["JokeID"] ." - Joke Question: ".$row["Joke_question"]." ".$row["Joke_answer"]."<br>";
-                }
-            }
-            else
-            {
-                echo "0 results";
-            }
+    include "db_connect.php";
+    $keywordFromForm = $_GET["keyword"];
+    echo $keywordFromForm;
+    // search database for the word stable
+    // pull data from test
+    echo "<h2>Show all jokes using $keywordFromForm</h2>";
+    $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table WHERE Joke_answer LIKE '%" . $keywordFromForm . "%'";
+    $result = $mysqli->query($sql);
+    if ($result->num_rows > 0)
+    {
+        // output data of each row
+        while ($row = $result->fetch_assoc())
+        {
+            echo "JokeID: ".$row["JokeID"] ." - Joke Question: ".$row["Joke_question"]." ".$row["Joke_answer"]."<br>";
+        }
+    }
+    else
+    {
+        echo "0 results";
+    }
 ?>
