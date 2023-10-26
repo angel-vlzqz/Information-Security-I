046fadbfc864b673ce5535033490eaed7a80c058
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-26 12:32:39 -0700

    format output page using jQuery UI

--- secureweb/search_keyword.php
+++ secureweb/search_keyword.php
@@ -1,18 +1,48 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>jQuery UI Accordion - Default functionality</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script>
+        $( function() {
+            $( "#accordion" ).accordion();
+        } );
+    </script>
+
+    <style>
+        *
+        {
+            font-family:    Arial, Helvetica, sans-serif;
+        }
+    </style>
+
+
+</head>
+
 <?php
     include "db_connect.php";
     $keywordFromForm = $_GET["keyword"];
     // search database for the word stable
     // pull data from test
-    echo "<h2>Show all jokes using $keywordFromForm</h2>";
+    echo "<h1>Show all jokes using $keywordFromForm<br></h1>";
     $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table WHERE Joke_answer LIKE '%" . $keywordFromForm . "%'";
+?>
 
+<div id="accordion">
+
+<?php
     $result = $mysqli->query($sql);
     if ($result->num_rows > 0)
     {
         // output data of each row
         while ($row = $result->fetch_assoc())
         {
-            echo "JokeID: ".$row["JokeID"] ." - Joke Question: ".$row["Joke_question"]." ".$row["Joke_answer"]."<br>";
+            // echo "JokeID: ".$row["JokeID"] ." - Joke Question: ".$row["Joke_question"]." ".$row["Joke_answer"]."<br>";
+            echo "<h3>$row[Joke_question]</h3>";
+            echo "<div><p>$row[Joke_answer]</p></div>";
         }
     }
     else
@@ -20,3 +50,5 @@
         echo "0 results";
     }
 ?>
+
+</div>
