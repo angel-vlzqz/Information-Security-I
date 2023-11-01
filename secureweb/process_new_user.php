351969cc6effbcb71e712e4aaa932697c72240a3
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-31 17:23:17 -0700

    add security feature: if a username is already taken, do not allow new username to enter the db

--- secureweb/process_new_user.php
+++ secureweb/process_new_user.php
@@ -1,11 +1,24 @@
 <?php
-    include "dv_connect.php";
+    include "db_connect.php";
     $newUsername = $_GET["username"];
     $newPassword1 = $_GET["password1"];
     $newPassword2 = $_GET["password2"];
 
     echo "<h2>Trying to add a new user " . $newUsername . " pw = " . $newPassword1 . " and " . $newPassword2 . "</h2>";
 
+    // check to see if the user already has an account
+    $sql = "SELECT * FROM Users_table WHERE Username = '$newUsername'";
+
+    $result = $mysqli->query($sql) or die(mysqli_error($mysqli));
+
+    // if there is a result, then the username is already taken
+    if ($result->num_rows > 0)
+    {
+        echo "<h2>Sorry, that username is already taken</h2>";
+        exit();
+    }
+
+    // insert a new user
     $sql = "INSERT INTO Users_table (UserID, Username, Password) VALUES (null, '$newUsername', '$newPassword1')";
 
     $result = $mysqli->query($sql) or die(mysqli_error($mysqli));
