ef709b685be2491e97c2d5d48d51f8a0308117e2
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-11-16 18:41:56 -0700

    add file to get and update user balance

---
+++ ACID/checkAccountDataService.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'autoLoader.php';
+
+class CheckAccountDataService
+{
+    function getBalance()
+    {
+        // get db connection
+        $db = new Database();
+        $conn = $db->getConnection();
+
+        // run query to get balance
+        $sql = "SELECT BALANCE FROM CHECKING";
+        $result = $conn->query($sql);
+
+        // if there is more than 0 rows, return the balance
+        if ($result->num_rows == 0)
+        {
+            // nothing found, return -1
+            $conn->close();
+            return -1;
+        }
+        else
+        {
+            // found the balance, return it
+            $row = $result->fetch_assoc();
+            $balance = $row["BALANCE"];
+            $conn->close();
+            return $balance;
+        }
+    }
+
+    function updateBalance($balance)
+    {
+
+    }
+}
