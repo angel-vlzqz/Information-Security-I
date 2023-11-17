b9ef6e62bce5f66edd967871e37006138463b42b
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-11-16 18:25:09 -0700

    add autoloader and database connection to ACID db

--- ACID/autoLoader.php
+++ ACID/autoLoader.php
@@ -1,8 +1,7 @@
+<?php
 function myAutoLoader($class)
 {
-require $class . '.php';
+    require $class . '.php';
 }
-splAutoLoadRegister('myAutoLoader');
 
-asdfasdf
-asdfasdf
+spl_autoload_register('myAutoLoader');
