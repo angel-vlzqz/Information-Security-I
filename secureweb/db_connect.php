41bc7e4151cca682dda7d31c96f63281f0c3c570
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-12-02 20:48:02 -0700

    add cloud hosting document

--- secureweb/db_connect.php
+++ secureweb/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 // connect to database
-$host = "localhost";
-$username = "root";
-$user_pass = "root";
+$host = "jokesmysql21.mysql.database.azure.com";
+$username = "jokesadmin";
+$user_pass = "bussybruh@21";
 $database_in_use = "test";
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
